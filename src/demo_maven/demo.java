2714b92f69dbe99a02f9e54bb5eac0e154fae0c0
indirachakka0702 <Indira1234@>
2020-05-06 22:55:23 -0600

    commiting to git

---
+++ src/demo_maven/demo.java
@@ -0,0 +1,32 @@
+package demo_maven;
+
+public class demo {
+
+	public static void main(String[] args) {
+		/*
+		 * Maven: -->Build /project management tool
+		 * how do we run .java file using command prompt
+		 * first you will compile it
+		 * you will run the file .class(binary) file
+		 * .jar files -->poi.jar -->
+		 * download the poi.jar --> we use to add manually to build path -->hectic
+		 * you cannot give your project to someone. However if you give project to someone. He may face lot of configuration error
+		 * 1% testers will use built tool
+		 * Build project--> .jar file
+		 * Install maven --> 1. In eclipse 2. To your command prompt
+		 * File --> new -->project --> maven 100% in all eclipse by default
+		 * 1.you can create a maven project -->
+		 * 2.convert existing java project to maven project
+		 * project name -->
+		 * lots of objects difining there
+		 * creating model by using all the above things
+		 * you have multiple java version installed in your machine -- 1.8, java 9, java 10
+		 * add dependency --> maven will take care rest
+		 * setting up of compiler version using plugin --> maven - compiler - version -->1.8 as my compiler version
+		 * really simplifies compiling problem and third party libraries problem, maintainence problem
+		 * maven - surefile -plugin --> selinium starts
+		 * 
+		 */
+	}
+
+}
